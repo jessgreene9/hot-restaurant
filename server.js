a1c4f524c0d19624991257f9e21ac1ae91899686
Jess Greene <jess.greene9@gmail.com>
2021-06-11 13:24:36 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Hot Restaurant');
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`listening at http://localhost:${PORT}`);
+});
